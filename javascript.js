979782ef541e5be73df2edc901132ca30f9ad843
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-01-31 15:45:39 -0700

    Add pseudocode

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+    // Return either rock, paper, or scissor randomly
+}
+
+function roundResult(playerSelection, computerSelection) {
+    // Make playerSelection case-insensitive
+    // Return string that declares the winner or tie
+}
+
+function playRound() {
+    // GET player's selection
+    // GET computer's selection
+    // CALL roundResult to display the result
+    // Return player's result
+}
+
+function playGame() {
+    // Initialize player's and computer's score as 0
+    // CALL playRound 5 times to play a five round game
+    // ADD 1 point to the winnner of each round
+    // DISPLAY the results of each round
+    // DISPLAY winners at the end
+}
+
